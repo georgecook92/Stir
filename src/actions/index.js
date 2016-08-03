35c2f06ed53cc48e9b49e083aa249dcc0e8a6ec6
George <george.cook.92@hotmail.co.uk>
2016-08-03 12:49:51 +0100

    remove notification for bug

--- src/actions/index.js
+++ src/actions/index.js
@@ -309,13 +309,13 @@ export function sendPost({title,text}) {
           } )
           .then( response => {
             console.log('response',response);
-            var message = {
-              app_id: '04954d84-8b33-4124-98cb-ac53f5abcf1d',
-              contents: {"en": "Recipe has been created"},
-              include_player_ids: ['f30be904-34c2-4d5d-8cc0-942806715c98']
-            };
-
-            sendNotification(message);
+            // var message = {
+            //   app_id: '04954d84-8b33-4124-98cb-ac53f5abcf1d',
+            //   contents: {"en": "Recipe has been created"},
+            //   include_player_ids: ['f30be904-34c2-4d5d-8cc0-942806715c98']
+            // };
+            //
+            // sendNotification(message);
 
             browserHistory.push('/posts/view');
           })
