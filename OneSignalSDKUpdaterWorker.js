fd5ca82590341dbac78f5fbaa77e06a57ee4a10e
George <george.cook.92@hotmail.co.uk>
2016-08-11 15:06:00 +0100

    changing order of SWs

--- OneSignalSDKUpdaterWorker.js
+++ OneSignalSDKUpdaterWorker.js
@@ -1,2 +1,2 @@
-importScripts('https://cdn.onesignal.com/sdks/OneSignalSDK.js');
 importScripts('https://stir-recipe.herokuapp.com/sw.js');
+importScripts('https://cdn.onesignal.com/sdks/OneSignalSDK.js');
