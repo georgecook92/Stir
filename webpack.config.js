18c85a0e22a3912861103f15d9624fc1fb95d4e1
George <george.cook.92@hotmail.co.uk>
2016-08-24 18:58:33 +0100

    trying to fix dev error in production

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,11 @@ module.exports = {
       }
     }]
   },
+  plugins: [
+    new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+}),
+  ],
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
